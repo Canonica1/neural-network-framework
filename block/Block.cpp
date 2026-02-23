d3dbfb373985aa31e37427d3152da2b7aaab844c
Vlad Romanovskii <shakeman78@gmail.com>
2026-02-23 15:17:00 +0300

    fuck

--- block/Block.cpp
+++ block/Block.cpp
@@ -1,49 +1,71 @@
+#include <memory>
+#include <utility>
 #include <vector>
 
-struct Concept {
-    virtual void apply() = 0;
-    virtual ~Concept() = default;
-};
+#include <Eigen/Dense>
+#include <folly/Poly.h>
 
-template<typename T>
-struct Model : public Concept { // тут надо подумать 
-public:
-    Model(const T &data) : data_(data) {
-    }
+using Matrix = Eigen::MatrixXf;
+
+struct ILayer {
+    template <class Base>
+    struct Interface : Base {
+        Matrix predict(const Matrix& x) const { return folly::poly_call<0>(*this, x); }
+        Matrix forward(const Matrix& x) { return folly::poly_call<1>(*this, x); }
+        Matrix backward(const Matrix& grad) { return folly::poly_call<2>(*this, grad); }
+        void update(float lr) { folly::poly_call<3>(*this, lr); }
+    };
+
+    template <class T>
+    using Members = folly::PolyMembers<
 
     
-    
-private:
-    T data_;
+        &T::predict,
+        &T::forward,
+        &T::backward,
+        &T::update>;
 };
 
-
-class Block {
-    Block() = default;
-
-};
+using Layer = folly::Poly<ILayer>;
 
 class NeuralNetwork {
     public:
-        void addLayer(Block block) {
-            blocks.push_back(block);
+        void addLayer(Layer block) {
+            blocks.push_back(std::move(block));
         }
-    private:
-        std::vector<Block> blocks;
+        Matrix predict(const Matrix &x) const {
+            Matrix result = x;
+            for (const auto& b: blocks) {
+                result = b.predict(result);
+            }
+            return result;
+        }
+   private:
+        std::vector<Layer> blocks;
 };
 
 class LinearLayer {
-
+public:
+    Matrix A;
+    Matrix B;
+    Matrix last_input;
+    LinearLayer(int in, int out)
+        : A(out, in),
+          B(out, 1) {
+        A.setZero();
+        B.setZero();
+    }
+    Matrix predict(const Matrix& prev) const {
+        return (A * prev) + B;
+    }
+    Matrix forward(const Matrix& prev) {
+        last_input = prev;
+        return (A * prev) + B;
+    }
+    Matrix backward(const Matrix& grad) {
+        return grad;
+    }
+    void update(float lr) {
+        (void)lr;
+    }
 };
-
-class SigmoidLayer {
-
-};
-
-int main() {
-    NeuralNetwork nn;
-    nn.addLayer(LinearLayer());
-    nn.addLayer(SigmoidLayer());
-    nn.addLayer(LinearLayer());
-    nn.addLayer(SigmoidLayer());
-}
